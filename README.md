0ea8f1f78f7a74ffe4a0ab8cbe1e9561b56f1410
Matias Dahl <matias.dahl@iki.fi>
2018-07-25 14:42:27 +0300

    add WorkBalancer

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # scala-apache-licensed
+
+Repository to store various Apache 2.0 licensed scala codes (from other projects), so
+that these can be kept separate from MIT licensed codes.
+
---
+++ src/main/scala/org/tagdynamics/sal/WorkBalancer.scala
@@ -0,0 +1,45 @@
+package org.tagdynamics.sal
+
+import akka.NotUsed
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+
+/**
+ * The `balancer` method below is copied from the Akka documentation, licensed under the
+ * Apache 2.0 license.
+ *
+ * See:
+ *  - https://github.com/akka/akka/blob/master/akka-docs/src/test/scala/docs/stream/cookbook/RecipeWorkerPool.scala
+ *  - https://doc.akka.io/docs/akka/2.5/stream/stream-cookbook.html
+ *  - https://blog.colinbreck.com/partitioning-akka-streams-to-maximize-throughput/
+ *
+ */
+
+object WorkBalancer {
+
+  //  --- start of copy pasted code (from first link above) ---
+
+  import akka.NotUsed
+  import akka.stream.FlowShape
+  import akka.stream.scaladsl._
+
+  def balancer[In, Out](worker: Flow[In, Out, Any], workerCount: Int): Flow[In, Out, NotUsed] = {
+    import GraphDSL.Implicits._
+
+    Flow.fromGraph(GraphDSL.create() { implicit b ⇒
+      val balancer = b.add(Balance[In](workerCount, waitForAllDownstreams = true))
+      val merge = b.add(Merge[Out](workerCount))
+
+      for (_ ← 1 to workerCount) {
+        // for each worker, add an edge from the balancer to the worker, then wire
+        // it to the merge element
+        balancer ~> worker.async ~> merge
+      }
+
+      FlowShape(balancer.in, merge.out)
+    })
+  }
+
+  //  --- end of copy pasted code ---
+
+}
